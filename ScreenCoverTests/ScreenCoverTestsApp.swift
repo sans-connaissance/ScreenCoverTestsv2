ba1bcca53d077a3429c00bb36acc396ae32f0d35
sans-connaissance <davemalicke@gmail.com>
2022-02-20 15:37:09 -0500

    Initial Commit

---
+++ ScreenCoverTests/ScreenCoverTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenCoverTestsApp.swift
+//  ScreenCoverTests
+//
+//  Created by David Malicke on 2/20/22.
+//
+
+import SwiftUI
+
+@main
+struct ScreenCoverTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
