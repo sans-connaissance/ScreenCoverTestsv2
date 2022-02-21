f86b097a442187f2ce0abdeb519f94b7cbf023e3
sans-connaissance <davemalicke@gmail.com>
2022-02-20 21:43:28 -0500

    FINISHED tutorial

--- ScreenCoverTests/ContentView.swift
+++ ScreenCoverTests/ContentView.swift
@@ -8,9 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var sheetMode: SheetMode = .none
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            FlexibleSheet(sheetMode: $sheetMode) {
+                VStack {
+                    Text("Hello World!")
+                        .foregroundColor(.white)
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color.green)
+                .clipShape(RoundedRectangle(cornerRadius: 25.0, style: .continuous))
+            }
+            
+            Button("Show") {
+                switch sheetMode {
+                case .none:
+                    sheetMode = .quarter
+                case .quarter:
+                    sheetMode = .half
+                case .half:
+                    sheetMode = .full
+                case .full:
+                    sheetMode = .none
+                }
+            }
+        }
     }
 }
 
